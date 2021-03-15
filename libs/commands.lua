aba5e917e91bf22a69f8abbbd82be2d6d0f620d8
RegularID <69537751+RegularID@users.noreply.github.com>
2021-03-15 20:02:57 +0200

    Create commands.lua

---
+++ libs/commands.lua
@@ -0,0 +1,46 @@
+local Commands = {}
+
+function Commands.add(Name, Function)
+    local Removed = false
+    local Command = {}
+    Command.Name = Name
+    Command.Description = ""
+    table.insert(Commands, Command);
+
+    function Command:descript(String)
+        if Removed then
+            error("attempt to call a nil value");
+        else
+            Command.Description = String
+        end
+    end
+
+    function Command:run(...)
+        local Args = {...}
+        if Removed then
+            error("attempt to call a nil value");
+        else
+            Function(Args);
+        end
+    end
+
+    function Command:remove()
+        Removed = true
+    end
+
+    return Command;
+end
+
+function Commands.list()
+    return unpack(Commands);
+end
+
+local output = Commands.add("print", function(Args)
+    print(unpack(Args));
+end);
+
+output:descript("prints out text");
+
+output:run("hi", "bye", 1);
+
+return Commands;
