bffe99206e75359341b73ea49c0c363d68d5e278
RegularID <69537751+RegularID@users.noreply.github.com>
2021-05-21 16:19:51 +0300

    Update Commands.lua

--- libs/Commands.lua
+++ libs/Commands.lua
@@ -13,5 +13,34 @@ function Commands.Add(Name, Aliases, Description, Function)
 
     return Command;
 end
+        
+function Commands.Check(Name, Prefix)
+    local Args = string.split(Name, " ")
+    local MessageCmd = table.remove(Args, 1)
+    Prefix = Prefix and Prefix or ""
+
+    for _, Command in ipairs(Commands) do
+        if Prefix .. string.lower(Command.Name) == string.lower(MessageCmd) then
+            local _, Error = pcall(function()
+                Command:Run(Args)
+                return Command.Name
+            end)
+            if Error then
+                return "Error"
+            end
+        end
+        for _, Alias in ipairs(Command.Aliases) do
+            if Prefix .. string.lower(Alias) == string.lower(MessageCmd) then
+                local _, Error = pcall(function()
+                    Command:Run(Args)
+                    return Command.Name, Alias
+                end)
+                if Error then
+                    return "Error"
+                end
+            end
+        end
+    end
+end
 
 return Commands;
