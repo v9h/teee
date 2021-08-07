0a31ac06701e27ab2a16e73357b4df9d13b09575
RegularID <69537751+RegularID@users.noreply.github.com>
2021-08-07 16:53:02 +0300

    Credits to Hunter
    updated the deprecated RunService events

--- libs/Run.lua
+++ libs/Run.lua
@@ -1,7 +1,7 @@
 local RunService = game:GetService("RunService")
 local Run = Instance.new("BindableEvent")
 
-for _, v in ipairs({RunService.RenderStepped, RunService.Stepped, RunService.Heartbeat}) do
+for _, v in ipairs({RunService.PreRender, RunService.PreSimulation, RunService.PostSimulation}) do
     v:Connect(function(...)
         Run:Fire(...)
     end)
