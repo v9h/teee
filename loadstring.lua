82f8630ef29840369177472bd3c565d452073904
Elden <69537751+RegularID@users.noreply.github.com>
2022-10-22 19:36:27 +0300

    Create loadstring.lua

---
+++ loadstring.lua
@@ -0,0 +1,35 @@
+local ScriptName = "The Streets" -- Script Name here; ex : The Streets
+
+
+local request = request or syn and syn.request
+
+
+local FilePath = string.gsub(ScriptName, "%s", "%%20")
+local RepositoryPath = "https://raw.githubusercontent.com/RegularID/roblox-the-streets-pony-script/main/" .. FilePath .. "/"
+
+
+local Cached = {}
+function import(Name: string, ...)
+    Name = string.gsub(Name, "%s", "%%20")
+    if Cached[Name] then return Cached[Name] end
+    
+    local Url = RepositoryPath .. Name .. ".lua"
+    local Response = request({Url = Url})
+    
+    local Source, Result = loadstring(Response.Body, Name)
+    if not Source then
+        return warn(Result)
+    end
+    
+    local Success, Result, Traceback = xpcall(Source, function(Error: string)
+        warn(string.format("[%s]: %s", File, string.match(Error, "%d+.+")))
+        return false, Error, debug.traceback()
+    end, ...)
+    if not Success then return end
+
+    Cached[Name] = Result
+    return Result
+end
+
+getgenv().import = import
+import "Source"
