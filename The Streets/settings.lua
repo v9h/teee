18c5bff74fa01d34c7067a163c3e493b45725659
Elden <69537751+elde-n@users.noreply.github.com>
2023-02-16 09:48:54 +0000

    Update settings.lua

--- The Streets/settings.lua
+++ The Streets/settings.lua
@@ -1,7 +1,7 @@
 return {
     name = "ponyhook",
     place_versions = {
-        [455366377] = 1565,
+        [455366377] = 1566,
         [4669040] = 225
     },
     config_directory = "ponyhook/Games/The Streets/"
