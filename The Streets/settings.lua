b48db0b52ac2aa05c8998319117c6be3db30e371
v9h <94395605+v9h@users.noreply.github.com>
2023-03-02 18:15:36 +0200

    Update settings.lua

--- The Streets/settings.lua
+++ The Streets/settings.lua
@@ -1,7 +1,7 @@
 return {
     name = "ponyhook",
     place_versions = {
-        [3472451742] = 1582,
+        [3472451732] = 1582,
         [4669040] = 225
     },
     config_directory = "ponyhook/Games/The Streets/"
